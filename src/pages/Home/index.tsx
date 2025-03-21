27d78fdb7cd695755f2cf13fd0732e9df3f52c50
Claudio Anjos <claudio.anjoss@outlook.com>
2025-03-21 17:38:40 -0300

    Remover arquivos desnecessários e refatorar lógica

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,9 @@
 import TableGC from '@/components/TableGC'
-import TestPage from '@/components/TestePage'
 
 export default function Home() {
   return (
     <main>
       <TableGC />
-      {/* <TestPage /> */}
     </main>
   )
 }
